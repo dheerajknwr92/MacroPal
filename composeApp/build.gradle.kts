e452e9104aa6c1515105b4260180786a7dc771fd
dheerajknwr92 <dheerajkanwar92@gmail.com>
2025-08-04 22:49:45 +0800

    Koin dependency added

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,8 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
